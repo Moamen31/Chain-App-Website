318b8de23fa7a4a17bfc68dd9b456cb343d95f92
Mo'men Ashraf <100002291+Moamen31@users.noreply.github.com>
2022-07-12 01:21:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //get elements
 let ul = document.querySelector(".ul")
 let btn = document.querySelector(".toggle")
+
 // the menu
 btn.addEventListener("click", function () {
     btn.classList.toggle("btn")
@@ -10,6 +11,8 @@ btn.addEventListener("click", function () {
 
 //scroll button
 let scrollBtn = document.querySelector(".scroll")
+//header
+let header = document.querySelector("header")
 
 //when to appear
 window.onscroll = function () {
@@ -19,25 +22,20 @@ window.onscroll = function () {
     else {
         scrollBtn.style.display = "none"
     }
+    //header to appear and be fixed
+    if (window.scrollY >= 400) {
+        header.style.cssText = "position:fixed;background-color:white;z-index:10;width:100%;"
+    }
+    if (window.scrollY === 0) {
+        header.style.cssText = "position:relative;background-color:transparent;z-index:10;width:100%;"
+    }
 }
 
 //function when clicked
 scrollBtn.addEventListener("click", function () {
     window.scrollTo({
-        top:0,
-        left:0,
-        behavior:"smooth",
+        top: 0,
+        left: 0,
+        behavior: "smooth",
     });
 });
-
-//header to appear and be fixed
-let header = document.querySelector("header")
-
-window.onscroll = function(){
-    if(window.scrollY >= 400){
-        header.style.cssText = "position:fixed;background-color:white;z-index:10;width:100%;"
-    }
-    if(window.scrollY === 0){
-        header.style.cssText = "position:relative;background-color:transparent;z-index:10;width:100%;"
-    }
-}
